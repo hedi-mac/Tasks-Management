d9fb8a6e5b389e89fe61ffc883221d00a9354778
hedi-mac <32374946+hedi-mac@users.noreply.github.com>
2023-09-19 19:12:05 +0100

    🌐 Adding Status code 🌐

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Depends, HTTPException
+from fastapi import FastAPI, Query, Depends, HTTPException, status
 from datetime import datetime, timedelta
 from typing import List, Annotated
 from database import engine, SessionLocal
@@ -9,8 +9,6 @@ import models
 app = FastAPI()
 models.Base.metadata.create_all(bind = engine)
 
-
-
 def get_db():
     db = SessionLocal()
     try:
@@ -20,7 +18,7 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.get('/tasks')
+@app.get('/tasks', status_code=status.HTTP_200_OK)
 async def get_all_tasks(
     db: db_dependency,
     page: int = Query(default=1, description="Page number, default is 1"),
@@ -45,30 +43,30 @@ async def get_all_tasks(
         query = query.filter(models.Tasks.finished_at <= finished_at_end)
     tasks = query.offset(offset).limit(per_page).all()
     if not tasks:
-        raise HTTPException(status_code=404, detail='Tasks not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='no tasks')
     return {'data': tasks}
 
-@app.get('/tasks/{id}')
+@app.get('/tasks/{id}', status_code=status.HTTP_200_OK)
 async def get_task_by_id(id: int, db: db_dependency):
     result = db.query(models.Tasks).filter(models.Tasks.id == id).first()
     if not result:
-        raise HTTPException(status_code=404, detail='task not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='task not found')
     return {'data': result}
 
-@app.put('/tasks/{id}/finished')
+@app.put('/tasks/{id}/finished', status_code=status.HTTP_202_ACCEPTED)
 async def set_task_finished(id: int, db: db_dependency):
     task = db.query(models.Tasks).filter(models.Tasks.id == id).first()
     if not task:
-        raise HTTPException(status_code=404, detail='task not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='task not found')
     task.update_status(True)
     db.commit()
     db.refresh(task)
     return {'data': task}
 
-@app.post("/tasks")
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
 async def create_tasks(task: TaskBase, db: db_dependency): 
     db_task = models.Tasks(title=task.title, description=task.description) 
-    db.add(db_task) 
+    db.add(db_task)
     db.commit() 
     db.refresh(db_task) 
 
