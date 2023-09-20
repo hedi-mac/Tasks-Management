fcd8e8679c301ab1f224a28bd6e715efb3b41a21
hedi-mac <32374946+hedi-mac@users.noreply.github.com>
2023-09-20 17:40:16 +0100

    🔗 Updating env variables 🔗

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = 'postgresql://postgres:1998@localhost:5432/TasksManagement'
+load_dotenv()
+
+URL_DATABASE = os.getenv("URL_DATABASE")
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
