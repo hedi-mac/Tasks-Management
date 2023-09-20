980720fca9f22d0456c30d9fad6d69ac752543d1
hedi-mac <32374946+hedi-mac@users.noreply.github.com>
2023-09-20 23:17:50 +0100

    🧪 Updating Unit tests 🧪

--- test_main.py
+++ test_main.py
@@ -8,27 +8,50 @@ from dateutil import parser
 
 client = TestClient(app)
 
-data = {
-  "title": "string",
-  "description": "string"
-}
+task_data = {"title": "New Task", "description": "This is a new task."}
+task_compare_data = {'title': 'New Task', 'description': 'This is a new task.', 'finished': False, 'finished_at': None, 'assigned_to': None}
+auth_data = {"username": "xxx", "password": "xxx"}
+auth_response = client.post("/login", data=auth_data)
+auth_token = auth_response.json().get("access_token")
 
 def test_create_task():
-    auth_data = {"username": "xxx", "password": "xxx"}
-    auth_response = client.post("/login", data=auth_data)
-    auth_token = auth_response.json().get("access_token")
-
-    data = {"title": "New Task", "description": "This is a new task."}
-
     response = client.post(
         "/tasks",
-        json=data,
-        headers={"Authorization": f"Bearer {auth_token}"},  # Add authorization header
+        json=task_data,
+        headers={"Authorization": f"Bearer {auth_token}"},  
     )
     assert response.json()["title"] == "New Task"
     assert response.json()["description"] == "This is a new task."
     created_at_str = response.json()["created_at"]
+
     created_at_datetime = parser.parse(created_at_str).astimezone(pytz.timezone('Africa/Tunis'))
-    current_datetime = datetime.datetime.now(pytz.timezone('Africa/Tunis'))  # Make it offset-aware
+    current_datetime = datetime.datetime.now(pytz.timezone('Africa/Tunis'))  
     assert created_at_datetime <= current_datetime
     assert response.status_code == 201
+
+def test_get_all_tasks():
+    response = client.get(
+        "/tasks",
+        headers={"Authorization": f"Bearer {auth_token}"},  
+    )
+    assert response.status_code == 200
+    assert task_compare_data == response.json()[-1]
+
+def test_set_task_finished():
+    response = client.put(
+        "/tasks/54/finished",
+        headers={"Authorization": f"Bearer {auth_token}"},
+    )
+    assert response.status_code == 202
+    assert response.json()["data"]["finished"]
+    created_at_datetime = parser.parse(response.json()["data"]["finished_at"]).astimezone(pytz.timezone('Africa/Tunis'))
+    current_datetime = datetime.datetime.now(pytz.timezone('Africa/Tunis'))  
+    assert created_at_datetime <= current_datetime
+
+def test_remove_task():
+    response = client.delete(
+        "/tasks/52",
+        headers={"Authorization": f"Bearer {auth_token}"},
+    )
+    assert response.status_code == 204
+    
